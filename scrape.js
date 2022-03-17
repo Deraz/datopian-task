e172f07c03a3062769eee0e9be4b2dc53c664613
ymostafa <ymostafa@rasan.co>
2022-03-17 18:25:22 +0200

    sorting data and finalizing writing technique

--- scrape.js
+++ scrape.js
@@ -29,32 +29,54 @@ request(
       const $ = cheerio.load(html);
       const table = $(".sortable");
       table
-        .find("tbody tr")
-        .slice(1)
+        .find("tbody tr") //Finding all rows
+        .slice(1) //Removing headers (First Row - rows[0])
         .each((i, row) => {
-          writeStream.write("\n");
+          //New line before each row
+          let stream = "\n";
+
           let newObj = {};
           let columnId = 0;
           $(row)
             .find("td")
+            //looping on columns for each row
             .each((j, td) => {
               if (j < 9 && j != 6) {
+                //Remove commas as it conflicts with CSVs, obviously.
                 let value = $(td).text().trim().replace(/,/g, "");
+
+                //Remove Special Character "†" and the following letter
                 let indexOfSpecialCharacter = value.indexOf("†");
                 if (indexOfSpecialCharacter > -1)
                   value = value.substring(0, indexOfSpecialCharacter);
-                newObj[cols[columnId].replace(/\s/g, "")] = value;
-                writeStream.write(`${value},`);
+
+                newObj[cols[columnId].replace(/\s/g, "")] =
+                  j == 0 ? value : parseInt(value);
+
+                stream += `${value},`;
                 columnId++;
               }
             });
-          writeStream.write("2018");
+
+          stream += "2018";
+          newObj.Year = 2018;
+          newObj.Stream = stream;
+
           data.push(newObj);
         });
 
-      console.log(data);
+      //Sorting data by Road death per million inhabitants
+      data
+        .sort(
+          (a, b) =>
+            a.RoadDeathPerMillionInhabitants - b.RoadDeathPerMillionInhabitants
+        )
+        //then looping on the sorted data to write in the CSV.
+        .forEach((row) => {
+          writeStream.write(row.Stream);
+        });
 
-      console.log("Scraping Done...");
+      console.log("Scraping Done..."); //Wohooo!
     }
   }
 );
